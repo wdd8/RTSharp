a577040feb05a24bea8488a35c64210fc53183f8
wdd8 <wdd@riseup.net>
2026-06-14 02:47:52 +0300

    Remove trim analyzer

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -17,40 +17,8 @@ dotnet publish ./src/plugins/RTSharp.ColoredRatio.Plugin -c Release -r $Rid --no
 dotnet publish ./src/rtorrent/RTSharp.DataProvider.Rtorrent.Plugin -c Release -r $Rid --no-self-contained -o "$(Join-Path $pwd $outDir 'plugins/DataProvider.Rtorrent')" /p:ScriptsDir="$(Join-Path $pwd 'scripts')/"
 dotnet publish ./src/qbittorrent/RTSharp.DataProvider.Qbittorrent.Plugin -c Release -r $Rid --no-self-contained -o "$(Join-Path $pwd $outDir 'plugins/DataProvider.Qbittorrent')" /p:ScriptsDir="$(Join-Path $pwd 'scripts')/"
 dotnet publish ./src/transmission/RTSharp.DataProvider.Transmission.Plugin -c Release -r $Rid --no-self-contained -o "$(Join-Path $pwd $outDir 'plugins/DataProvider.Transmission')" /p:ScriptsDir="$(Join-Path $pwd 'scripts')/"
-rm -r "$outDir/clidriver" -ErrorAction SilentlyContinue
-rm "$outDir/TestConn.*" -ErrorAction SilentlyContinue
+
 rm "$outDir/*.xml" -ErrorAction SilentlyContinue
-rm "$outDir/Azure.*.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Extensions.MySql.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Extensions.Oracle.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Extensions.Postgres.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Extensions.Snowflake.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Extensions.SqlServer.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Db2.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Firebird.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Hana.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.MySql.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Oracle.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Postgres.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Redshift.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.Snowflake.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FluentMigrator.Runner.SqlServer.dll" -ErrorAction SilentlyContinue
-rm "$outDir/FirebirdSql.Data.FirebirdClient.dll" -ErrorAction SilentlyContinue
-rm "$outDir/IBM.Data.Db2.dll" -ErrorAction SilentlyContinue
-rm "$outDir/Microsoft.Data.SqlClient.dll" -ErrorAction SilentlyContinue
-#rm "$outDir/Microsoft.Data.SqlClient.SNI.dll"
-rm "$outDir/Microsoft.IdentityModel.*.dll" -ErrorAction SilentlyContinue
-rm "$outDir/Microsoft.Identity.*.dll" -ErrorAction SilentlyContinue
-rm "$outDir/Microsoft.SqlServer.Server.dll" -ErrorAction SilentlyContinue
-rm "$outDir/Microsoft.Bcl.AsyncInterfaces.dll" -ErrorAction SilentlyContinue
-rm "$outDir/Microsoft.Bcl.Cryptography.dll" -ErrorAction SilentlyContinue
-rm "$outDir/System.ClientModel.dll" -ErrorAction SilentlyContinue
-rm "$outDir/System.Configuration.ConfigurationManager.dll" -ErrorAction SilentlyContinue
-#rm "$outDir/System.Diagnostics.EventLog.dll"
-rm "$outDir/System.IdentityModel.Tokens.Jwt.dll" -ErrorAction SilentlyContinue
-rm "$outDir/System.Memory.Data.dll" -ErrorAction SilentlyContinue
-#rm "$outDir/System.Security.Cryptography.Pkcs.dll"
-rm "$outDir/System.Security.Cryptography.ProtectedData.dll" -ErrorAction SilentlyContinue
 
 rm "$outDir/plugins/*/*.xml" -ErrorAction SilentlyContinue
 rm "$outDir/plugins/*/*.runtimeconfig.json" -ErrorAction SilentlyContinue
